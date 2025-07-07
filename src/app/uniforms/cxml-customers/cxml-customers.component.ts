a7b03d19908477fc68ccdd65ae03c2a077532ffd
DelgaJ <delgadoj0226@gmail.com>
2025-07-07 10:45:58 -0400

    UP 2.0 CXML Customers LM fixes

--- src/app/uniforms/cxml-customers/cxml-customers.component.ts
+++ src/app/uniforms/cxml-customers/cxml-customers.component.ts
@@ -51,14 +51,14 @@ export class CxmlCustomersComponent {
       if (this.page.data?.title) this.page.title = this.page.data.title;
       if (this.page.data?.fullname) this.page.fullname = this.page.data.fullname;
       if (this.page.data?.menu) this.page.menu = this.page.data.menu;
-      if (this.page.data?.total) this.total = this.page.data.total
+      if (this.page.data?.total) this.total = this.page.data.total;
       hideWait();
       this.page.loading = false;
     });
   }
 
   filterConfigs(event: any){
-    this.acno = event.target.value
+    this.acno = event
     this.getConfigs();
   }
 
@@ -67,7 +67,7 @@ export class CxmlCustomersComponent {
   }
 
   newConfig(){
-      this.router.navigate(['/uniforms/cxmlcustomer/' + this.page.rfno]);
+    this.router.navigate(['/uniforms/cxmlcustomer/' + this.page.rfno]);
   }
 
   onItemChange(event: number){

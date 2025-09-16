25f8f053b1df63a86df951e61dce012fcb5ef00b
DelgaJ <delgadoj0226@gmail.com>
2025-09-16 14:57:02 -0400

    UP 2.0 NV2 Adjustments

--- src/app/uniforms/vas-questions/vas-questions.component.ts
+++ src/app/uniforms/vas-questions/vas-questions.component.ts
@@ -124,10 +124,24 @@ export class VasQuestionsComponent {
 
       if(temp.data.rules.length > 0){
         for (let i = 0; i < temp.data.rules.length; i++) {
-          rules[i] = temp.data.rules[i].ques + ',' + temp.data.rules[i].drop + ',' +  temp.data.rules[i].dfan + ',' + temp.data.rules[i].dflk
+          let rule = temp.data.rules[i].ques
+          if(temp.data.rules[i].drop) rule += ( ',' + temp.data.rules[i].drop)
+          if(temp.data.rules[i].dfan) rule += ( ',' + temp.data.rules[i].dfan)
+          if(temp.data.rules[i].dflk) rule += ( ',' + temp.data.rules[i].dflk)
+          rules.push(rule)
+        }
+        this.getQuestions(rules, temp.data.questions);
+      } else if(temp.data.rulesV.length > 0){
+        for (let i = 0; i < temp.data.rulesV.length; i++) {
+          let rule = temp.data.rulesV[i].ques
+          if(temp.data.rulesV[i].drop) rule += ( ',' + temp.data.rulesV[i].drop)
+          if(temp.data.rulesV[i].dfan) rule += ( ',' + temp.data.rulesV[i].dfan)
+          if(temp.data.rulesV[i].dflk) rule += ( ',' + temp.data.rulesV[i].dflk)
+          rules.push(rule)
         }
         this.getQuestions(rules, temp.data.questions);
       }
+
 
       } else{
         if (mode !== 'validate') this.msg = "Questions updated successfully"
@@ -137,12 +151,12 @@ export class VasQuestionsComponent {
 
         if(mode == 'validate'){
           for (let x = 0; x < temp.data?.questions.length; x++){
-            if(temp.data?.questions[x]?.rules){
-              for (let i = 0; i < temp.data.questions[x].rules.length; i++) {
-                let rule = temp.data.questions[x].rules[i].ques
-                if(temp.data.questions[x].rules[i].drop) rule += ( ',' + temp.data.questions[x].rules[i].drop)
-                if(temp.data.questions[x].rules[i].dfan) rule += ( ',' + temp.data.questions[x].rules[i].dfan)
-                if(temp.data.questions[x].rules[i].dflk) rule += ( ',' + temp.data.questions[x].rules[i].dflk)
+            if(temp.data?.questions[x]?.rulesV){
+              for (let i = 0; i < temp.data.questions[x].rulesV.length; i++) {
+                let rule = temp.data.questions[x].rulesV[i].ques
+                if(temp.data.questions[x].rulesV[i].drop) rule += ( ',' + temp.data.questions[x].rulesV[i].drop)
+                if(temp.data.questions[x].rulesV[i].dfan) rule += ( ',' + temp.data.questions[x].rulesV[i].dfan)
+                if(temp.data.questions[x].rulesV[i].dflk) rule += ( ',' + temp.data.questions[x].rulesV[i].dflk)
                 rules.push(rule)
               }
             }

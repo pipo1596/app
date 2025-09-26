249a942554c067e1c6e32dca8c3eab2c08e32ed9
DelgaJ <delgadoj0226@gmail.com>
2025-09-26 11:26:21 -0400

    UP 2.0 Product SR bug fix

--- src/app/uniforms/product/product.component.ts
+++ src/app/uniforms/product/product.component.ts
@@ -407,14 +407,23 @@ export class ProductComponent {
     this.op2U = []
     this.op3U = []
     for(let i = 0; i < this.page.data.option1.length; i++){
-      if(!this.opv[0]?.includes(this.page.data.option1[i])){
-        this.op1U.push(this.page.data.option1[i])
+
+      if(this.page.data?.option1){
+        if(!this.opv[0]?.includes(this.page.data?.option1[i])){
+          this.op1U.push(this.page.data?.option1[i])
+        }
       }
-      if(!this.opv[1]?.includes(this.page.data.option2[i])){
-        this.op2U.push(this.page.data.option2[i])
+
+      if(this.page.data?.option2){
+        if(!this.opv[1]?.includes(this.page.data?.option2[i])){
+          this.op2U.push(this.page.data?.option2[i])
+        }
       }
-      if(!this.opv[2]?.includes(this.page.data.option3[i])){
-       this.op3U.push(this.page.data.option3[i])
+
+      if(this.page.data?.option3){
+        if(!this.opv[2]?.includes(this.page.data?.option3[i])){
+        this.op3U.push(this.page.data?.option3[i])
+        }
       }
     }
     this.op1U = this.op1U.join(',');

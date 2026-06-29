8a593ecd3b35e83ec9e0eb77112a110494554d8f
DelgadoJ <delgadoj0226@gmail.com>
2026-06-29 12:00:21 -0400

    UP 2.0 Phase 2 Fixes 5

--- src/app/uniforms/iframe/iframe.component.ts
+++ src/app/uniforms/iframe/iframe.component.ts
@@ -22,34 +22,18 @@ export class IframeComponent {
 
   constructor(private router: Router, private route: ActivatedRoute, private sanitizer: DomSanitizer) { }
 
-@HostListener('window:message', ['$event']) onMessage(event: MessageEvent) {
-  if (!event || !event.data || !event.data.data) {
-    return;
-  }
-
-  const payload = String(event.data.data);
-
-  if (this.partpg && ((this.partpg.indexOf('customization') !== -1) || (this.partpg.indexOf('newcustomization') !== -1))  ) {
-    if (payload.includes('APOELMIS')) {
-      return; 
+  @HostListener('window:message', ['$event']) onMessage(event: MessageEvent) {
+    localStorage.setItem('UP_AUTH','Y');
+    localStorage.setItem('expanded',this.exp);
+    if(this.p1 && this.partpg?.indexOf('newuniform') == -1){
+      this.p1 = JSON.parse(this.p1)
+      if(this.p1?.vedp && this.partpg?.indexOf('newvasapplication') !== -1) this.p1.vedp = '';
+      this.p1 = JSON.stringify(this.p1)
     }
+    if(this.p1) localStorage.setItem('p1',this.p1);
+    if(this.p2) localStorage.setItem('p2',this.p2);
+    this.router.navigate([this.partpg + event.data.data]);
   }
-
-  localStorage.setItem('UP_AUTH', 'Y');
-  localStorage.setItem('expanded', this.exp);
-  if (this.p1 && this.partpg?.indexOf('newuniform') == -1) {
-    this.p1 = JSON.parse(this.p1);
-    if (this.p1?.vedp && this.partpg?.indexOf('newvasapplication') !== -1) {
-      this.p1.vedp = '';
-    }
-    this.p1 = JSON.stringify(this.p1);
-  }
-
-  if (this.p1) localStorage.setItem('p1', this.p1);
-  if (this.p2) localStorage.setItem('p2', this.p2);
-
-  this.router.navigate([this.partpg + event.data.data]);
-}
 
   ngOnInit(): void {
     showWait();
